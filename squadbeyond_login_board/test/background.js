a41d8f1c501ba0bdfa9a20164f58325e763a2662
DC-hashimoto-naoki <hashimoto_naoki@discovery-inc.com>
2022-08-29 10:50:33 +0900

    update extensions

--- squadbeyond_login_board/test/background.js
+++ squadbeyond_login_board/test/background.js
@@ -12,11 +12,11 @@
             });
         })
         console.log(name + ' ' + email);
-        return {name: name, email: email};
+        return { name: name, email: email };
     }
 
     function sendLogoutData(name, email) {
-        const sheetUrl = "https://script.google.com/macros/s/AKfycbxsY_AURotkCYowsKsgHM-bzhtcaCNqStPB-ARcMrZnDufnoCeIx8SdaDheNhrLHsCCvQ/exec";
+        const sheetUrl = "https://script.google.com/macros/s/AKfycbz3b95D0DV3Lids06x0vSWcJYf3X3C1hVK1qVwZSeYDg7rgbBNh_tsLtNbPhX4CLglCfg/exec";
 
         // タブを閉じたときにデータ送信
         var tabToUrl = {};
@@ -33,7 +33,7 @@
             let url = tabToUrl[tabId];
             if (url.match(pattern)) {
                 console.log('remove tab in squadbeyond');
-                const login = false;
+                const login = ""; // 空白はfalsyな値
                 let emailAdress = email;
                 let sendUrl = sheetUrl + "?name=" + name + "&emailAdress=" + emailAdress + "&login=" + login;
                 try {
@@ -56,4 +56,11 @@
             sendLogoutData(name, email);
         })
 
+
+    // on install
+    chrome.runtime.onInstalled.addListener(function () {
+        console.log("now installed");
+        
+
+    })
 })();
--- squadbeyond_login_board/test/content.js
+++ squadbeyond_login_board/test/content.js
@@ -1,9 +1,9 @@
-(function() {
+(function () {
     console.log('content.js is runnning');
 
     const inputEmail = document.querySelector('[type="email"]')
     const inputSubmit = document.querySelector('[type="submit"]');
-    const url = "https://script.google.com/macros/s/AKfycbxsY_AURotkCYowsKsgHM-bzhtcaCNqStPB-ARcMrZnDufnoCeIx8SdaDheNhrLHsCCvQ/exec";
+    const url = "https://script.google.com/macros/s/AKfycbz3b95D0DV3Lids06x0vSWcJYf3X3C1hVK1qVwZSeYDg7rgbBNh_tsLtNbPhX4CLglCfg/exec";
 
     let name = '匿名';
     // storage処理
@@ -31,5 +31,29 @@
     }
 
     sendLoginData();
+
+    function alertLoginMessage() {
+        let message = "";
+        alert('現在のログイン');
+        console.log('alertFuc');
+
+        const url = "https://sheets.googleapis.com/v4/spreadsheets/";
+        const sheetId = "1kfmhj6z-NlZAfG_EGtpv8dMbz0LI5ADCXKvs9o7BJJM";
+        const api = "AIzaSyATQbgbd1EL0C2po7-fBYTcAC0T3rk6mkQ";
+        const endpoint = url + sheetId + '/values/シート1?key=' + api;
+
+        try {
+            fetch(endpoint)
+                .then(response => response.json())
+                .then(data => {
+                    console.table(data.values);
+                })
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    alertLoginMessage();
 
 }());
